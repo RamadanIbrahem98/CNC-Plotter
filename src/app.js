0bb97e2bd91ef9b797d734b10376897e4aa24ffd
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2022-06-07 17:17:02 +0200

    feat: add server functionallity

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const emitter = new EventEmitter();
 let server = express();
 const { Logger } = require('./logger');
+const { exec } = require('child_process');
+const fs = require('fs');
 
 server.use(cors());
 server.use(express.json());
@@ -16,14 +18,45 @@ server.use((req, res, next) => {
   next();
 });
 
+let chip;
+const devices = [];
+
+server.post('/image', async (req, res, next) => {
+  const { buffer } = req.body;
+  const fileName = await savePicture(buffer);
+  image2GCode(fileName);
+
+  res.send({ msg: 'succeed' });
+});
+
+server.post('/pre-set', (req, res, next) => {
+  const { shape } = req.body;
+  console.log(shape);
+  let index = 0;
+  switch (shape) {
+    case 'square':
+      index = 0;
+      break;
+    case 'triangle':
+      index = 2;
+      break;
+    case 'face':
+      index = 3;
+      break;
+  }
+
+  if (chip) {
+    chip.send(index, { binary: false });
+  }
+
+  res.send({ msg: 'success' });
+});
+
 server = server.listen(process.env.PORT || 80, () =>
   logger.info('Server', `Server started on port ${process.env.PORT || 80}`),
 );
 
 const wss = new Server({ server });
-
-let chip;
-const devices = [];
 
 emitter.on('sendGCode', () => {
   // TODO: Create the Logic to send the binary file to ESP
@@ -32,6 +65,40 @@ emitter.on('sendGCode', () => {
     chip.available = false;
   }
 });
+
+const savePicture = async (base64Image) => {
+  const fileExtention = base64Image.split(';')[0].split('/')[1];
+  const fileName = `image.${fileExtention}`;
+  const base64Data = base64Image.split(',')[1];
+  await fs.writeFile(`./${fileName}`, base64Data, 'base64', (err) => {
+    if (err) {
+    }
+  });
+
+  return fileName;
+};
+
+const image2GCode = (imageName) => {
+  console.log(imageName);
+  exec(
+    `python image2GCode/image_to_gcode.py -i ${imageName} -o gcode.nc`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+    },
+  );
+  exec(
+    `python image2GCode/gcode_parser.py -i gcode.nc -b bin --use-g -s 200x200`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+    },
+  );
+};
 
 wss.on('connection', function connection(ws, req) {
   switch (req.url) {
@@ -60,15 +127,27 @@ wss.on('connection', function connection(ws, req) {
           ws.send({ msg: 'ESP is busy' }, { binary: false });
           return;
         }
-        logger.input('Slave', data);
         data = await JSON.parse(data);
 
         if (data.type === 'text') {
           // TODO: Run the text to GCode As a child process
         } else if (data.type === 'picture') {
           // TODO: Save the picture to disk and run the picture to GCode As a child process
+          const fileName = savePicture(data.buffer);
+          image2GCode(fileName);
         } else if (data.type === 'pre-set') {
           // TODO: Send the Character Directly to ESP
+          switch (data.buffer) {
+            case 'triangle':
+              drawTriangle();
+              break;
+            case 'square':
+              drawSquare();
+              break;
+            case 'circle':
+              drawCircle();
+              break;
+          }
         }
 
         emitter.emit('sendGCode');
